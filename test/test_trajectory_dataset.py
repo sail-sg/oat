5489d4bfbc3ab8694753a2d7fb156bd2b52ca954
Zichen <liuzc@sea.com>
2025-07-23 22:35:41 +0800

    breaking: add multi-turn trajectory data and rename the original to transition data

---
+++ test/test_trajectory_dataset.py
@@ -0,0 +1,60 @@
+# Copyright 2025 Garena Online Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test multi-turn trajectory dataset."""
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+
+from oat.types import TrajectoryData
+from oat.utils.data import TrajectoryDataset
+from oat.utils.deepspeed import DummyStrategy
+
+
+def test():
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-0.5B")
+    dataset = load_dataset("robinsmits/ChatAlpaca-20K")["test"]
+
+    ds = TrajectoryDataset(
+        [
+            TrajectoryData(
+                trajectory_ids=None,
+                num_turns=None,
+                response_token_ranges=None,
+                turn_weights=None,
+                messages=x,
+            )
+            for x in dataset["messages"]
+        ],
+        tokenizer,
+        DummyStrategy(None),
+    )
+
+    dl = DataLoader(
+        ds,
+        batch_size=len(ds),
+        shuffle=(False),
+        drop_last=True,
+        pin_memory=True,
+        collate_fn=ds.collate_fn,
+    )
+
+    data = next(iter(dl))
+    datum = {k: v[0] for k, v in data.items()}
+    for i, (st, end) in enumerate(datum["response_token_ranges"]):
+        print(f"turn-{i}", tokenizer.decode(datum["input_ids"][st:end]))
+
+
+if __name__ == "__main__":
+    test()
